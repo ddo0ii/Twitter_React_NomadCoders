69a8c83d3fe5cedab051498bd489c1bede34f72e
ddo0ii <soyeong5857@gmail.com>
2022-10-01 16:29:41 +0900

    3.1 Nweeting!

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,27 +1,35 @@
 import React, { useState } from "react";
+import { dbService } from "fbase";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 
 const Home = () => {
-  const [sweet, setSweet] = useState("");
-  const onSubmit = (event) => {
-    event.preventDefault();
+  const [nweet, setNweet] = useState("");
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log(`서브밋 하는 느윗:${nweet}`);
+    await addDoc(collection(dbService, "nweets"), {
+      nweet,
+      createdAt: serverTimestamp(),
+    });
+    setNweet("");
   };
   const onChange = (event) => {
     const {
       target: { value },
     } = event;
-    setSweet(value);
+    setNweet(value);
   };
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input
-          value={sweet}
+          value={nweet}
           onChange={onChange}
           type="text"
           placeholder="What's on your mind?"
           maxLength={120}
         />
-        <input type="submit" value="Sweet" />
+        <input type="submit" value="Nweet" />
       </form>
     </div>
   );
