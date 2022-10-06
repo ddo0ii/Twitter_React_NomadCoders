7f18a23b3efc43aff4ae6564ce588ebb940ab252
ddo0ii <soyeong5857@gmail.com>
2022-10-06 15:33:21 +0900

    5.0 Get My Own Nweets

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,16 +1,42 @@
-import React from "react";
-import { authService } from "fbase";
+import React, { useEffect } from "react";
+import { authService, dbService } from "fbase";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
 import { useHistory } from "react-router-dom";
 
-export default () => {
+//1. 로그인한 유저 정보 prop으로 받기
+const Profile = ({ userObj }) => {
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+
+  //2. 내 nweets 얻는 function 생성
+  const getMyNweets = async () => {
+    //3. 트윗 불러오기
+    //3-1. dbService의 컬렉션 중 "nweets" Docs에서 userObj의 uid와 동일한 creatorID를 가진 모든 문서를 내림차순으로 가져오는 쿼리(요청) 생성
+    const q = query(
+      collection(dbService, "nweets"),
+      where("creatorId", "==", userObj.uid),
+      orderBy("createdAt", "desc")
+    );
+    //3-2. getDocs()메서드로 쿼리 결과 값 가져오기
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, "=>", doc.data());
+    });
+  };
+
+  //4. 내 nweets 얻는 function 호출
+  useEffect(() => {
+    getMyNweets();
+  }, []);
+
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
 };
+
+export default Profile;
